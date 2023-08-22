897bcbff31b1ec45e42ab77aaac7cf8f1eaac392
yuan <yuan@hotmail.com>
2023-08-23 00:23:44 +0800

    Anika animation

--- app/Anika.tsx
+++ app/Anika.tsx
@@ -2,17 +2,20 @@
 
 import { useEffect } from 'react';
 import * as THREE from 'three';
-import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { Color, Mesh, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { clipBase_Layer, clipBlink, clipHello } from './config';
 
 export const Anika = () => {
     useEffect(() => {
         const sceneDom = document.getElementById('scene');
         const rect = sceneDom?.getBoundingClientRect()!;
         console.log(rect);
+
+        const clock = new THREE.Clock();
 
         const scene = new Scene();
         const camera = new PerspectiveCamera( 15, rect.width / rect.height, 1, 20 );
@@ -35,29 +38,93 @@ export const Anika = () => {
         scene.background = new THREE.Color( 0x666666 );
         scene.environment = pmremGenerator.fromScene( environment ).texture;
 
-        // const controls = new OrbitControls( camera, renderer.domElement );
-        // controls.enableDamping = true;
-        // controls.minDistance = 2.5;
-        // controls.maxDistance = 5;
-        // controls.minAzimuthAngle = - Math.PI / 2;
-        // controls.maxAzimuthAngle = Math.PI / 2;
-        // controls.maxPolarAngle = Math.PI / 1.8;
-
         scene.add( new THREE.AxesHelper( 20 ) );
+        let Anika010Mixer: THREE.AnimationMixer;
+        let rigMixer: THREE.AnimationMixer;
+        let rigAction: THREE.AnimationAction;
+        let blinkAction: THREE.AnimationAction;
 
-        function animate() {
-            requestAnimationFrame( animate );    
-            renderer.render( scene, camera );
+        const loadingManager = new THREE.LoadingManager();
+        loadingManager.onLoad = function() {
+            if(rigAction) {
+                rigAction.fadeIn(0.1).play();
+                setTimeout(()=>{
+                    rigAction.crossFadeTo(blinkAction, 2, !0),
+                    blinkAction.play()
+                })
+            }
+
+            if(Anika010Mixer) {
+                const action = Anika010Mixer.clipAction(clipBase_Layer);
+                action.setLoop(THREE.LoopRepeat, 10);
+                action.timeScale = .8;
+                action.play()
+            }
         }
 
-        animate();
-
-        new GLTFLoader().setMeshoptDecoder( MeshoptDecoder )
+        new GLTFLoader(loadingManager).setMeshoptDecoder( MeshoptDecoder )
             .load('/anika.glb', (gltf) => {
                 console.log(gltf);
 
                 scene.add(gltf.scene);
+
+                const rig = gltf.scene.getObjectByName("Fem-A_Anika_RIG");
+                console.log('rig', rig);
+                rigMixer = new THREE.AnimationMixer( rig! );
+                rigAction = rigMixer.clipAction(clipHello);
+                rigAction.setLoop(THREE.LoopOnce, 1);
+                blinkAction = rigMixer.clipAction(clipBlink);
+                
+                const Anika010 = gltf.scene.getObjectByName('Fem-A_Whl_BY_Anika010')! as Mesh;
+                //@ts-ignore
+                Anika010.material.depthWrite = !0;
+                Anika010.castShadow = !0;
+                Anika010.receiveShadow = !0;
+
+                const Anika010_1 = gltf.scene.getObjectByName("Fem-A_Whl_BY_Anika010_1")! as Mesh;
+                const Anika010_5 = gltf.scene.getObjectByName("Fem-A_Whl_BY_Anika010_5")! as Mesh;
+                const Anika010_4 = gltf.scene.getObjectByName("Fem-A_Whl_BY_Anika010_4")! as Mesh;
+                const Anika010_3 = gltf.scene.getObjectByName("Fem-A_Whl_BY_Anika010_3")!;
+                const mesh_7 = gltf.scene.getObjectByName("mesh_7")!;
+                const c_tong_01x = gltf.scene.getObjectByName("c_tong_01x")!;
+
+                //@ts-ignore
+                mesh_7.material.color = new Color(5912613);
+
+                //@ts-ignore
+                // Anika010_3.material.envMap = s;
+                //@ts-ignore
+                Anika010_3.material.envMapIntensity = 3;
+
+                //@ts-ignore
+                Anika010_1.material.opacity = .3;
+                //@ts-ignore
+                Anika010_1.material.color = new Color(5912613);
+                //@ts-ignore
+                Anika010_4.material.opacity = .2;
+                //@ts-ignore
+                Anika010_4.material.color = new Color(5912613);
+                mesh_7.receiveShadow = !0;
+                Anika010_5.receiveShadow = !0;
+                Anika010_5.castShadow = !0;
+                c_tong_01x.translateZ(-.0025);
+
+                Anika010Mixer = new THREE.AnimationMixer(Anika010);
+                Anika010Mixer.clipAction(clipBase_Layer).setLoop(THREE.LoopOnce, 1);
+
+                const P = Anika010.morphTargetInfluences;
+                Anika010_1.morphTargetInfluences = P;
+                Anika010_5.morphTargetInfluences = P;
+                Anika010_4.morphTargetInfluences = P;
+            });
+
+            renderer.setAnimationLoop(() => {
+                const p = clock.getDelta();
+                Anika010Mixer && Anika010Mixer.update(p);
+                rigMixer && rigMixer.update(p);
+                renderer.render( scene, camera );
             })
+    
     }, [])
 
     return <div id="scene" className='w-full h-full min-h-full shrink relative transition-transform duration-500'></div>
